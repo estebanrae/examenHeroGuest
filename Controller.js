82e48d5cbe2017b6161696a4a487b18d73efa96f
Esteban Rae <stv.rhcp@gmail.com>
2018-09-03 22:31:14 -0500

    ultimo commit

--- Controller.js
+++ Controller.js
@@ -11,15 +11,18 @@ appgeneral.controller("ctrl", function($scope, $http){
   
 	$scope.cambiarPag = function(clase, titulo){
 		$scope.titulo = titulo;
+		$scope.tituloPost = '';
 		$(".pantalla").each(function(){
 			$(this).hide();
 		});
 		$(".pantallas").find("." + clase).fadeIn(200);
 		if(clase=='lista'){
 			$(".mensaje-error").hide();
+			$scope.instrucciones = "Da click en un post para ver detalles";
 			init($scope);
 		}	
 		if(clase=='detalles'){
+			$scope.instrucciones = '';
 			$scope.tituloPost = this.x.title;
 			$scope.contenidoPost = this.x.body;
 			insertarComentarios($scope, this.x.id);
@@ -33,6 +36,7 @@ appgeneral.controller("ctrl", function($scope, $http){
 			url = 'https://jsonplaceholder.typicode.com/posts';
 			init($scope);
 		}else{
+			$(".loader").show();
 			console.log(url);
 			fetch(url)
 			  .then(function(response){
@@ -49,30 +53,53 @@ appgeneral.controller("ctrl", function($scope, $http){
 			  	$scope.posts = [];
 			  	$scope.posts.push(J);
 			  	$scope.$apply();
-
+			  	$(".loader").hide();
 			  });
+		}
+		
+	}
+
+	$scope.agregarPost = function(){
+		$(".mensaje").hide();
+		if(!$scope.tituloInput || !$scope.contenidoInput){
+			$(".mensaje-error-enviar").fadeIn(300);
+		}else{
+			$http({
+				url: 'https://jsonplaceholder.typicode.com/posts',
+				method: "POST",
+				data: {"title": $scope.tituloInput, "body": $scope.contenidoInput, "userId": 1}
+				}).then(function(s){
+					$scope.idNuevo = s.data.id;
+					$(".mensaje-exito-enviar").show();
+					console.log(s);
+				}, function(err){
+					console.log(err);
+			});
 		}
 		
 	}
 });
 
 function init($scope){
+	$(".loader").show();
 	fetch('https://jsonplaceholder.typicode.com/posts')
   .then(function(response){
   	return response.json();
   }).then(function(J){
   	$scope.posts = J;
   	$scope.$apply();
+  	$(".loader").hide();
   });
 }
 
 function insertarComentarios($scope, id){
+	$(".loader").show();
 	fetch('https://jsonplaceholder.typicode.com/posts/' + id +'/comments')
   .then(function(response){
   	return response.json();
   }).then(function(J){
   	$scope.comentarios = J;
   	$scope.$apply();
-  	console.log(J);
+  	$(".loader").hide();
   });
 }
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include("header.html");?>
 <div class="contenedor-principal" ng-app="app" ng-controller="ctrl">
-	<p class="titulo">{{titulo}}</p>
+	<p class="titulo">{{titulo}}{{tituloPost}}</p>
 	<div class="botones">
 		<button class="active" ng-click="cambiarPag('lista', 'Lista de Posts')">Lista de Posts</button>
 		<button ng-click="cambiarPag('nuevo', 'Agregar Nuevo Post')">Agregar nuevo post</button>
@@ -17,15 +17,15 @@
 				</p>
 				<p>
 					<label>Buscar por ID:</label>
-					<input ng-model="searchID">
-					<button ng-click="buscarServidor()">Buscar en Servidor</button>
+					<input ng-model="searchID" ng-keyup="buscarServidor()">
+					<span class="loader"></span>
 				</p>
 				
 			</div>
 			<p class="mensaje-error inactive">Su busqueda no generó resultados</p>
 			<table>
-				<tr><th>Titulo</th></tr>
-				<tr class="posts" ng-repeat="x in posts | filter:searchText "><td ng-click="cambiarPag('detalles', 'Detalles')" data-id="{{x.id}}">{{x.title}}</td></tr>
+				<tr><th>Posts</th></tr>
+				<tr class="posts" ng-repeat="x in posts | filter:searchText "><td ng-click="cambiarPag('detalles', 'Detalles de: ')" data-id="{{x.id}}">{{x.title}}</td></tr>
 			</table>
 
 		</div><!-- pantalla-1 -->
@@ -36,13 +36,36 @@
 				<label>Descripcion</label>
 				<p ng-bind="contenidoPost"></p>
 			</div>
+			<span class="loader"></span>
 			<div class="contenedor-comentarios">
 				<label>Comentarios</label>
-				<p ng-repeat="x in comentarios">{{x.name}}</p>
+				<div class="comentario" ng-repeat="x in comentarios">
+
+					<span class="titulos">
+						<p>{{x.name}}</p>
+						<p class="mail">{{x.email}}</p>
+					</span>
+					<div class="contenido-comentario">
+						<p>{{x.body}}</p>	
+					</div>
+				
+				</div>
 			</div>
 		</div><!-- pantalla-2 -->
 		<div class="pantalla nuevo">
-			Nuevo
+			<form class="formulario">
+				<p>
+				<label>Titulo</label>
+				<input ng-model="tituloInput" />
+				</p>
+				<p>
+				<label>Contenido</label>
+				<textarea  ng-model="contenidoInput" ></textarea>
+				</p>
+				<button ng-click="agregarPost()">Guardar</button>
+			</form>
+			<div class=" mensaje mensaje-error-enviar">Por favor llene todos los campos</div>
+			<div class=" mensaje mensaje-exito-enviar" >Se ha agregado el POST con ID: <span ng-bind="idNuevo"></span></div>
 		</div><!-- pantalla-3 -->
 	</div>
 
